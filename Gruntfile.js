4665efda9368f5d3176a34ddbf79567678325c70
msodeveloper <m.soares2603@gmail.com>
2014-10-30 15:23:33 -0200

    start using bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,8 @@ module.exports = function(grunt) {
     'grunt-contrib-jshint',
     'grunt-contrib-concat',
     'grunt-contrib-watch',
-    'grunt-contrib-sass'
+    'grunt-contrib-sass',
+    'grunt-contrib-cssmin'
   ];
 
   var config = {};
