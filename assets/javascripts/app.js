ae89ff812c4c8b2d829dbcfd28474d40ff0dfb7a
Mauricio Soares <m.soares2603@gmail.com>
2014-11-01 12:09:27 -0200

    notifications tested! yay

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -252,6 +252,10 @@ $(function() {
 } (this));
 
 (function(root) {
+  // constants
+  var TIMEOUT = 1000000,
+    FADEOUT_TIMEOUT = 500;
+
   var Notifications = function($content) {
     // creates a element in the dom for notifications
     this.$container = $('<div class="notifications">');
@@ -267,16 +271,24 @@ $(function() {
     this.$container.prepend($notification);
 
     this.addEvents($notification);
+
+    return $notification;
   };
 
   Notifications.prototype.addEvents = function($notification) {
-    setTimeout(this.destroy.bind(this, $notification), 3000);
+    setTimeout(this.destroy.bind(this, $notification), TIMEOUT);
     $notification.find('a').on('click', this.destroy.bind(this, $notification));
   };
 
   Notifications.prototype.destroy = function($notification) {
     // checks if the element is still in the dom
-    if($notification.closest(document.documentElement)) $notification.fadeOut(500, function() { $notification.remove(); });
+    // and destroys it
+    if($notification.closest(document.documentElement)) $notification.fadeOut(FADEOUT_TIMEOUT, function() { $notification.remove(); });
+  };
+
+  // kinda helper for testing
+  Notifications.prototype.destroyAll = function() {
+    if(this.$container.find('.notification')) this.$container.empty();
   };
 
   root.Notifications = Notifications;
---
+++ tests/notifications/notificationsSpec.js
@@ -0,0 +1,56 @@
+describe('Notifications Constructor', function() {
+  var $content = $('<div>'),
+    notifications = new Notifications($content)
+
+  afterEach(function() {
+    notifications.destroyAll();
+  });
+
+  it('should append the .notifications div inside the $content', function() {
+    expect($content.has('.notifications').length).toEqual(1);
+  });
+
+  it('should append a new notification', function() {
+    notifications.new(5);
+    expect($content.find('.notification').length).toEqual(1);
+  });
+
+  it('should accept multiple notifications', function() {
+    notifications.new(5);
+    notifications.new(10);
+    notifications.new(15);
+    expect($content.find('.notification').length).toEqual(3);
+  });
+
+  it('should show the parameter number inside the content', function() {
+    notifications.new(3457);
+    expect($content.find('.notification').text()).toEqual('Item 3457 was deleted×');
+  });
+
+  it('should destroy a specific notification', function() {
+    var notification1 = notifications.new(5);
+    var notification2 = notifications.new(10);
+
+    notifications.destroy(notification1);
+
+    expect($content.find('.notification').length).toEqual(1);
+    expect($content.find('.notification').text()).toEqual('Item 10 was deleted×');
+  });
+
+  it('should call destroy when you click in the close button', function() {
+    spyOn(notifications, 'destroy');
+
+    var notification = notifications.new(5);
+    notification.find('a').click();
+
+    expect(notifications.destroy).toHaveBeenCalled();
+  });
+
+  // it('should destroy a notification after an timeout of 3000 miliseconds', function(done) {
+  //   notifications.new(3457);
+  //   setTimeout(function() {
+  //     expect($content.find('.notification').length).toEqual(0);
+  //     done();
+  //   }, 3001);
+  // });
+});
